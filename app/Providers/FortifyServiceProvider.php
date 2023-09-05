306dcea2b3654ea77ce6acfc7959dbd44b64621b
Irfan Ullah <iukhan53@gmail.com>
2023-09-05 18:40:53 +0500

    small changes

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -15,32 +15,39 @@ use Laravel\Fortify\Fortify;
 
 class FortifyServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        Fortify::createUsersUsing(CreateNewUser::class);
-        Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    Fortify::createUsersUsing(CreateNewUser::class);
+    Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
+    Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
+    Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
 
-        RateLimiter::for('login', function (Request $request) {
-            $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())).'|'.$request->ip());
+    Fortify::loginView(function () {
+      return view('auth.login', ['pageConfigs' => ['myLayout' => 'blank']]);
+    });
+    Fortify::registerView(function () {
+      return view('auth.register', ['pageConfigs' => ['myLayout' => 'blank']]);
+    });
 
-            return Limit::perMinute(5)->by($throttleKey);
-        });
+    RateLimiter::for('login', function (Request $request) {
+      $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())) . '|' . $request->ip());
 
-        RateLimiter::for('two-factor', function (Request $request) {
-            return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        });
-    }
+      return Limit::perMinute(5)->by($throttleKey);
+    });
+
+    RateLimiter::for('two-factor', function (Request $request) {
+      return Limit::perMinute(5)->by($request->session()->get('login.id'));
+    });
+  }
 }
