dea6e505bea2c00cbe56ddbbac8400d8ed9dbf9b
Irfan Ullah <iukhan53@gmail.com>
2023-09-12 23:03:23 +0500

    footer changes

--- resources/views/layouts/landing/footer.blade.php
+++ resources/views/layouts/landing/footer.blade.php
@@ -1,11 +1,4 @@
 <div class="wrapper" id="wrapper-footer">
-
-  <style>
-    #wrapper-footer:after {
-      display: none !important;
-    }
-  </style>
-
   <div class="container">
     <div class="footer-widgets__top justify-content- gap-2">
       <ul class="footer-widgets__block footer-widgets__top--left">
@@ -42,15 +35,73 @@
         </li>
       </ul>
       <ul class="footer-widgets__block footer-widgets__top--right">
-        <li id="text-6" class="widget footer-widgets__top--demo widget_text">
-          <div class="textwidget"><p>Shrink your workload.<br>
+        <li id="text-6" class="widget footer-widgets__top--demo widget_text" style="background: #373a9a">
+          <div class="textwidget">
+            <p>Shrink your workload.<br>
               Save your workforce.</p>
-            <p><a class="btn btn-lg btn-blue btn-primary"
-                  href="{{route('book-demo')}}">Book a Demo</a></p>
+            <p>
+              <a class="btn btn-lg btn-blue btn-primary"
+                 href="{{route('book-demo')}}">Book a Demo</a>
+            </p>
           </div>
         </li>
       </ul>
-
     </div>
-  </div><!-- container end -->
+    <div class="footer-widgets__bottom">
+      <ul class="footer-widgets__block footer-widgets__bottom--left">
+        <li id="media_image-2" class="widget  widget_media_image">
+          <a href="/">
+            <img width="110" height="50" src="{{asset('landing/assets/img/logo.png')}}"
+                 class="image wp-image-164 attachment-medium size-medium entered lazyloaded" alt="">
+          </a>
+        </li>
+      </ul>
+
+      <ul class="footer-widgets__block footer-widgets__bottom--middle">
+        <li id="nav_menu-9" class="widget  widget_nav_menu">
+          <div class="menu-footer-legal-container">
+            <ul id="menu-footer-legal-1" class="menu">
+              <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-152"><a
+                  href="#">Privacy Policy</a></li>
+              <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-153"><a
+                  href="#">Terms of Use</a></li>
+              <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-154"><a
+                  href="#">Cookie Policy</a></li>
+              <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-155"><a
+                  href="#">EULA</a></li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+      <ul class="footer-widgets__block footer-widgets__bottom--right">
+        <li id="nav_menu-10" class="widget  widget_nav_menu">
+          <div class="menu-footer-social-container">
+            <ul id="menu-footer-social" class="menu">
+              <li id="menu-item-156"
+                  class="view-icon-only menu-item menu-item-type-custom menu-item-object-custom menu-item-156">
+                <a href="#">
+                  <img width="32" height="32" src="https://www.hyro.ai/wp-content/uploads/2022/12/Mask-group-1-1.svg"
+                       alt="">
+                </a>
+              </li>
+              <li id="menu-item-157"
+                  class="view-icon-only menu-item menu-item-type-custom menu-item-object-custom menu-item-157">
+                <a href="#">
+                  <img width="32" height="32" src="https://www.hyro.ai/wp-content/uploads/2022/12/Mask-group-2.svg"
+                       alt="">
+                </a>
+              </li>
+              <li id="menu-item-158"
+                  class="view-icon-only menu-item menu-item-type-custom menu-item-object-custom menu-item-158">
+                <a href="#">
+                  <img width="32" height="32" src="https://www.hyro.ai/wp-content/uploads/2022/12/Mask-group-3.svg"
+                       alt="">
+                </a>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
 </div>
