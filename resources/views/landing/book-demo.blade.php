6ac277fac34a852d2f18081b5ebdb64577bfabd4
Irfan Ullah <iukhan53@gmail.com>
2023-09-24 17:38:01 +0500

    client side email added

--- resources/views/landing/book-demo.blade.php
+++ resources/views/landing/book-demo.blade.php
@@ -12,36 +12,39 @@
         <div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-6da175d"
              data-id="6da175d" data-element_type="column">
           <div class="elementor-widget-wrap elementor-element-populated">
-{{--            <div class="elementor-element elementor-element-1f3b2b3 elementor-hidden-tablet elementor-hidden-mobile elementor-widget elementor-widget-image"--}}
-{{--                 data-id="1f3b2b3" data-element_type="widget" data-widget_type="image.default">--}}
-{{--              <img decoding="async" width="261" height="133"--}}
-{{--                   src="{{asset('landing/assets/img/book-now-svg.svg')}}" alt="">--}}
-{{--            </div>--}}
-{{--            <div class="elementor-element elementor-element-8799aec elementor-hidden-desktop elementor-widget elementor-widget-image"--}}
-{{--                 data-id="8799aec" data-element_type="widget" data-widget_type="image.default">--}}
-{{--              <div class="elementor-widget-container">--}}
-{{--                <img decoding="async" width="135" height="101"--}}
-{{--                     src="{{asset('landing/assets/img/book-now-svg-mobile.svg')}}" alt="">--}}
-{{--              </div>--}}
-{{--            </div>--}}
+            {{--            <div class="elementor-element elementor-element-1f3b2b3 elementor-hidden-tablet elementor-hidden-mobile elementor-widget elementor-widget-image"--}}
+            {{--                 data-id="1f3b2b3" data-element_type="widget" data-widget_type="image.default">--}}
+            {{--              <img decoding="async" width="261" height="133"--}}
+            {{--                   src="{{asset('landing/assets/img/book-now-svg.svg')}}" alt="">--}}
+            {{--            </div>--}}
+            {{--            <div class="elementor-element elementor-element-8799aec elementor-hidden-desktop elementor-widget elementor-widget-image"--}}
+            {{--                 data-id="8799aec" data-element_type="widget" data-widget_type="image.default">--}}
+            {{--              <div class="elementor-widget-container">--}}
+            {{--                <img decoding="async" width="135" height="101"--}}
+            {{--                     src="{{asset('landing/assets/img/book-now-svg-mobile.svg')}}" alt="">--}}
+            {{--              </div>--}}
+            {{--            </div>--}}
             <section
               class="elementor-section elementor-inner-section elementor-element elementor-element-134684a elementor-section-full_width elementor-section-content-middle elementor-section-height-default elementor-section-height-default"
               data-id="134684a" data-element_type="section">
               <div class="elementor-container elementor-column-gap-no">
-                <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-e95f5ba"
-                     data-id="e95f5ba" data-element_type="column">
+                <div
+                  class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-e95f5ba"
+                  data-id="e95f5ba" data-element_type="column">
                   <div class="elementor-widget-wrap elementor-element-populated">
-                    <div class="elementor-element elementor-element-cd7e217 d-h3 m-h1 elementor-widget elementor-widget-heading"
-                         data-id="cd7e217" data-element_type="widget"
-                         data-widget_type="heading.default">
+                    <div
+                      class="elementor-element elementor-element-cd7e217 d-h3 m-h1 elementor-widget elementor-widget-heading"
+                      data-id="cd7e217" data-element_type="widget"
+                      data-widget_type="heading.default">
                       <div class="elementor-widget-container">
                         <h1 class="elementor-heading-title elementor-size-default">More
                           conversions come<br>
                           from <i>better</i> conversations.</h1></div>
                     </div>
-                    <div class="elementor-element elementor-element-e04cc47 elementor-widget elementor-widget-text-editor"
-                         data-id="e04cc47" data-element_type="widget"
-                         data-widget_type="text-editor.default">
+                    <div
+                      class="elementor-element elementor-element-e04cc47 elementor-widget elementor-widget-text-editor"
+                      data-id="e04cc47" data-element_type="widget"
+                      data-widget_type="text-editor.default">
                       <div class="elementor-widget-container">
                         <p>Get a customized Medvenix demo based on your own data, without lifting a
                           finger. Experience your key business use cases in action from the
@@ -49,8 +52,9 @@
                     </div>
                   </div>
                 </div>
-                <div class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-dea0ba0 hyro-form-wrapper hyro-form-wrapper--light"
-                     data-id="dea0ba0" data-element_type="column">
+                <div
+                  class="elementor-column elementor-col-50 elementor-inner-column elementor-element elementor-element-dea0ba0 hyro-form-wrapper hyro-form-wrapper--light"
+                  data-id="dea0ba0" data-element_type="column">
                   <div class="elementor-widget-wrap elementor-element-populated">
                     <div class="elementor-element elementor-element-d7a1109 elementor-widget elementor-widget-heading"
                          data-id="d7a1109" data-element_type="widget"
@@ -59,49 +63,48 @@
                         <h6 class="elementor-heading-title elementor-size-default">Book a
                           One-on-One Demo</h6></div>
                     </div>
-                    <div class="elementor-element elementor-element-446cc2e elementor-button-align-stretch elementor-form-style--multi_field elementor-form-color--dark elementor-form-button--pink elementor-form-button--filled elementor-widget elementor-widget-form">
+                    <div
+                      class="elementor-element elementor-element-446cc2e elementor-button-align-stretch elementor-form-style--multi_field elementor-form-color--dark elementor-form-button--pink elementor-form-button--filled elementor-widget elementor-widget-form">
                       <div class="elementor-widget-container">
-                        <form class="elementor-form" method="post" name="Demo Form"
-                              novalidate="novalidate">
-                          <input type="hidden" name="post_id" value="8">
-                          <input type="hidden" name="form_id" value="446cc2e">
-                          <input type="hidden" name="referer_title"
-                                 value="Request a Demo - Medvenix.ai">
-
-                          <input type="hidden" name="queried_id" value="8">
-
+                        <form class="elementor-form" method="post">
                           <div class="elementor-form-fields-wrapper elementor-labels-above">
-                            <div class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-firstname elementor-col-50 elementor-field-required">
-                              <input size="1" type="text" name="form_fields[firstname]"
+                            <div
+                              class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-firstname elementor-col-50 elementor-field-required">
+                              <input size="1" type="text" name="firstname"
                                      id="form-field-firstname"
                                      class="elementor-field elementor-size-sm  elementor-field-textual"
                                      placeholder="First Name" required>
                             </div>
-                            <div class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-lastname elementor-col-50 elementor-field-required">
+                            <div
+                              class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-lastname elementor-col-50 elementor-field-required">
                               <input size="1" type="text" name="form_fields[lastname]"
                                      id="form-field-lastname"
                                      class="elementor-field elementor-size-sm  elementor-field-textual"
                                      placeholder="Last Name" required>
                             </div>
-                            <div class="elementor-field-type-email elementor-field-group elementor-column elementor-field-group-email elementor-col-100 elementor-field-required">
+                            <div
+                              class="elementor-field-type-email elementor-field-group elementor-column elementor-field-group-email elementor-col-100 elementor-field-required">
                               <input size="1" type="email" name="form_fields[email]"
                                      id="form-field-email"
                                      class="elementor-field elementor-size-sm  elementor-field-textual"
                                      placeholder="Work Email" required>
                             </div>
-                            <div class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-jobtitle elementor-col-100 elementor-field-required">
+                            <div
+                              class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-jobtitle elementor-col-100 elementor-field-required">
                               <input size="1" type="text" name="form_fields[jobtitle]"
                                      id="form-field-jobtitle"
                                      class="elementor-field elementor-size-sm  elementor-field-textual"
                                      placeholder="Job Title" required>
                             </div>
-                            <div class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-company elementor-col-100 elementor-field-required">
+                            <div
+                              class="elementor-field-type-text elementor-field-group elementor-column elementor-field-group-company elementor-col-100 elementor-field-required">
                               <input size="1" type="text" name="form_fields[company]"
                                      id="form-field-company"
                                      class="elementor-field elementor-size-sm  elementor-field-textual"
                                      placeholder="Company" required>
                             </div>
-                            <div class="elementor-field-type-select elementor-field-group elementor-column elementor-field-group-how_did_you_hear_about_us_ elementor-col-100">
+                            <div
+                              class="elementor-field-type-select elementor-field-group elementor-column elementor-field-group-how_did_you_hear_about_us_ elementor-col-100">
                               <select name="form_fields[how_did_you_hear_about_us_]"
                                       id="form-field-how_did_you_hear_about_us_"
                                       class="elementor-field-textual elementor-size-sm">
@@ -117,8 +120,9 @@
                               </select>
                             </div>
 
-                            <div class="elementor-field-group elementor-column elementor-field-type-submit elementor-col-100 e-form__buttons">
-                              <button type="submit" class="btn btn-primary btn-blue btn-lg w-100">
+                            <div
+                              class="elementor-field-group elementor-column elementor-field-type-submit elementor-col-100 e-form__buttons">
+                              <button type="button" class="btn btn-primary btn-blue btn-lg w-100" onclick="sendEmail();">
                                 Let's Talk
                               </button>
                             </div>
@@ -135,4 +139,25 @@
       </div>
     </section>
   </div>
+  <script>
+    function sendEmail() {
+      const firstName = document.getElementById("form-field-firstname").value;
+      const lastName = document.getElementById("form-field-lastname").value;
+      const email = document.getElementById("form-field-email").value;
+      const jobTitle = document.getElementById("form-field-jobtitle").value;
+      const company = document.getElementById("form-field-company").value;
+      const howDidYouHear = document.getElementById("form-field-how_did_you_hear_about_us_").value;
+
+      const subject = encodeURIComponent("Demo Request from " + firstName + " " + lastName);
+      const body = encodeURIComponent(
+        `First Name: ${firstName}\n` +
+        `Last Name: ${lastName}\n` +
+        `Email: ${email}\n` +
+        `Job Title: ${jobTitle}\n` +
+        `Company: ${company}\n` +
+        `How Did You Hear About Us: ${howDidYouHear}`
+      );
+      window.location.href = `mailto:medvenix@info.com?subject=${subject}&body=${body}`;
+    }
+  </script>
 @endsection
